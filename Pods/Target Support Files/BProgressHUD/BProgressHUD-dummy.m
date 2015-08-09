d2fd54c07cb188ed9c8eb3e930fbd77de9e7d968
Ant <antonyharfield@gmail.com>
2015-08-09 16:44:20 +0700

    Remove pods.

--- Pods/Target Support Files/BProgressHUD/BProgressHUD-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_BProgressHUD : NSObject
-@end
-@implementation PodsDummy_BProgressHUD
-@end
--- Pods/Target Support Files/Pods-SciWeek58/Pods-SciWeek58-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_SciWeek58VersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_SciWeek58VersionString[];
-
